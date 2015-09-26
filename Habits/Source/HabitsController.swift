45111918923c56942c79cc86051ce0b1f7ea3ae0
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-26 23:33:32 +0200

    Add `HabitsController` table view controller

---
+++ Habits/Source/HabitsController.swift
@@ -0,0 +1,11 @@
+//
+//  HabitsController.swift
+//  Habits
+//
+//  Created by Julian Grosshauser on 26/09/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class HabitsController: UITableViewController {}
