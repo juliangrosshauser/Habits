ee1a28ae26d7521d98170624ce5e96b2cbcee9a9
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-26 23:44:08 +0200

    Add `HabitsViewModel` view model

---
+++ Habits/Source/HabitsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HabitsViewModel.swift
+//  Habits
+//
+//  Created by Julian Grosshauser on 26/09/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+class HabitsViewModel {}
