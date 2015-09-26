81d9e07e609f88f79bd90186783e9abadb553dcc
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-26 23:45:43 +0200

    Initialize `HabitsController` with view model

--- Habits/Source/AppDelegate.swift
+++ Habits/Source/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        let navigationController = UINavigationController(rootViewController: HabitsController())
+        let navigationController = UINavigationController(rootViewController: HabitsController(viewModel: HabitsViewModel()))
 
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
