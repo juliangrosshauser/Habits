d02f0450bcb71ac83a8b900aa51c64e4f40cdb87
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-27 20:35:06 +0200

    Register `HabitsCell` in table view

--- Habits/Source/HabitsController.swift
+++ Habits/Source/HabitsController.swift
@@ -23,4 +23,11 @@ class HabitsController: UITableViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    //MARK: UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(HabitsCell.self, forCellReuseIdentifier: String(HabitsCell))
+    }
 }
