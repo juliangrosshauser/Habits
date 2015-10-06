6c518e54c94f0d506c184ef53d03f134221a0757
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-06 23:35:38 +0200

    Add `StoreError` enumeration

---
+++ Habits/Source/StoreError.swift
@@ -0,0 +1,11 @@
+//
+//  StoreError.swift
+//  Habits
+//
+//  Created by Julian Grosshauser on 06/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+enum StoreError: ErrorType {
+    case PrimaryKeyDoesntExist
+}
