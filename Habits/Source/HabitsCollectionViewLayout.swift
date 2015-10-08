c6fd19fe6c081a262433a4094c0eb63c44cd3080
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-08 23:49:30 +0200

    Add `HabitsCollectionViewLayout` class

---
+++ Habits/Source/HabitsCollectionViewLayout.swift
@@ -0,0 +1,11 @@
+//
+//  HabitsCollectionViewLayout.swift
+//  Habits
+//
+//  Created by Julian Grosshauser on 05/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class HabitsCollectionViewLayout: UICollectionViewFlowLayout {}
